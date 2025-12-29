5e7093b2ebcce6b0a521149a5697967279e43df8
Snehal <your-email@example.com>
2025-12-29 18:13:00 +0530

    Python code

---
+++ If_Loop_Logical_Operator.py
@@ -0,0 +1,18 @@
+
+if 5>2 and 7>3:
+    print("If Statement with Logical Operator")
+print("Rest of the Code")
+
+print("------------------------------------------------------------------") 
+
+
+if 5>2 or 1>3:
+    print("If Statement with Logical Operator - OR")
+    print("Statement 2")
+print("Rest of the Code")
+
+
+ 
+
+
+     
---
+++ Nested_if_Loop.py
@@ -0,0 +1,10 @@
+
+if 5>2:
+    print("Greater")
+    print("5 is greater than 2")
+    if(6>2):
+        print("6 is greater than 2")
+print("Rest of the Code")
+
+print("------------------------------------------------------------------") 
+     
