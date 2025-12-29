da9168a5e67873fbac4b6214fbe1fcf76ad4b7e4
Snehal <your-email@example.com>
2025-12-29 15:24:22 +0530

    Change the code

---
+++ Type_Conversion.py
@@ -0,0 +1,33 @@
+# Implicit Type Conversion 
+
+a = 5
+b = 2
+value = a / b
+print(value)
+print(type(value))
+
+j ="Hello"
+k ="python"
+result = j + k
+print(result)
+print(type(result))
+
+# m = 20
+# n = 'Snehal'
+# t = m + n
+# print(t)
+
+# Explicit Type Conversion
+
+a = 5
+b = 2
+value = a / b
+
+print(type(value))
+int_value = int(value)
+print(type(int_value))
+
+print(int_value)
+
+
+
