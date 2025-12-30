74141cd95003cb0097578143e5d0bba592f8f8eb
Snehal <your-email@example.com>
2025-12-30 17:59:19 +0530

    Python code

---
+++ Array.py
@@ -0,0 +1,20 @@
+
+from array import *
+stu_roll =array('i',[101,102,103,104,105])
+
+for i in stu_roll:
+    print(i)
+
+
+print("------------------------------------------------------------------")
+
+from array import *
+stu_roll = array('i',[101,102,103,104,105])
+n = len(stu_roll)
+
+for i in range(n):
+    print(i, "=", stu_roll[i])
+    
+print(n)
+
+
